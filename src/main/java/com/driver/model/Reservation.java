36c6bae7203278972a9c9bd6cf363ed55b2035ef
Ramya <mutyamramya0101@gmail.com>
2023-06-23 22:36:45 +0530

    heyy

---
+++ src/main/java/com/driver/model/Reservation.java
@@ -0,0 +1,72 @@
+package com.driver.model;
+
+import javax.persistence.*;
+
+@Entity
+public class Reservation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    @Column(name = "number_of_hours")
+    private int numberOfHours;
+    @ManyToOne
+    @JoinColumn
+    private User user;
+    @ManyToOne
+    @JoinColumn
+    private Spot spot;
+    @OneToOne
+    @JoinColumn
+    private Payment payment;
+
+    public Reservation(int id, int numberOfHours, User user, Spot spot, Payment payment) {
+        this.id = id;
+        this.numberOfHours = numberOfHours;
+        this.user = user;
+        this.spot = spot;
+        this.payment = payment;
+    }
+
+    public Reservation() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getNumberOfHours() {
+        return numberOfHours;
+    }
+
+    public void setNumberOfHours(int numberOfHours) {
+        this.numberOfHours = numberOfHours;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Spot getSpot() {
+        return spot;
+    }
+
+    public void setSpot(Spot spot) {
+        this.spot = spot;
+    }
+
+    public Payment getPayment() {
+        return payment;
+    }
+
+    public void setPayment(Payment payment) {
+        this.payment = payment;
+    }
+}
--- src/main/java/com/driver/services/impl/ReservationServiceImpl.java
+++ src/main/java/com/driver/services/impl/ReservationServiceImpl.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class ReservationServiceImpl implements ReservationService {
@@ -23,6 +24,69 @@ public class ReservationServiceImpl implements ReservationService {
     ParkingLotRepository parkingLotRepository3;
     @Override
     public Reservation reserveSpot(Integer userId, Integer parkingLotId, Integer timeInHours, Integer numberOfWheels) throws Exception {
+        //Reserve a spot in the given parkingLot such that the total price is minimum.
+        // Note that the price per hour for each spot is different
+        //Note that the vehicle can only be parked in a spot having a type equal to or larger than given vehicle
+        //If parkingLot is not found, user is not found, or no spot is available, throw "Cannot make reservation" exception.
+        Reservation reservation = new Reservation();
 
+        if (userRepository3.findById(userId) == null) {
+            throw new Exception("Cannot make reservation");
+        }
+        User user = userRepository3.findById(userId).get();
+
+        if (parkingLotRepository3.findById(parkingLotId) == null) {
+            throw new Exception("Cannot make reservation");
+        }
+        ParkingLot parkingLot = parkingLotRepository3.findById(parkingLotId).get();
+
+        List<Spot> spotList = parkingLot.getSpotList();
+        Spot optionalSpot = null;
+        int optimalPrice = Integer.MAX_VALUE;
+        for (Spot spot : spotList) {
+            if (spot.getOccupied() == false) {
+                if (spot.getSpotType().equals(SpotType.TWO_WHEELER))
+                    if (numberOfWheels <= 2) {
+                        if (optimalPrice > spot.getPricePerHour()) {
+                            optimalPrice = spot.getPricePerHour();
+                            optionalSpot = spot;
+                        }
+                    }
+            }
+            else if (spot.getSpotType().equals(SpotType.FOUR_WHEELER)) {
+                if (numberOfWheels <= 4) {
+                    if (optimalPrice > spot.getPricePerHour()) {
+                        optimalPrice = spot.getPricePerHour();
+                        optionalSpot = spot;
+                    }
+                }
+            }
+            else {
+                if (optimalPrice > spot.getPricePerHour()) {
+                    optimalPrice = spot.getPricePerHour();
+                    optionalSpot = spot;
+                }
+            }
+        }
+        if (optionalSpot==null){
+            throw new Exception("Cannot make reservation");
+        }
+        optionalSpot.setOccupied(true);
+
+        reservation.setNumberOfHours(timeInHours);
+        reservation.setUser(user);
+        reservation.setSpot(optionalSpot);
+
+        List<Reservation> reservations=user.getReservationList();
+        List<Reservation> reservationList=optionalSpot.getReservationList();
+
+        reservations.add(reservation);
+        reservationList.add(reservation);
+
+        user.setReservationList(reservationList);
+        optionalSpot.setReservationList(reservationList);
+        userRepository3.save(user);
+        spotRepository3.save(optionalSpot);
+        return reservation;
     }
 }
